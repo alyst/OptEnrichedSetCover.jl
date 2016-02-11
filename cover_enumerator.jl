08d9dcb0b715f59c690b9614f136867713108428
Alexey Stukalov <astukalov@gmail.com>
2016-02-11 01:49:23 +0100

    [*] CoverEnumerator: fixes & test for min set score

--- cover_enumerator.jl
+++ cover_enumerator.jl
@@ -21,22 +21,18 @@ immutable CoverCollection{T,S}
     end
 end
 
-function Base.push!(covers::CoverCollection, cover::CoverProblemResult, problem::CoverProblem)
-    delta_score = isempty(covers.variants) ? 0.0 : cover.score - covers.variants[1].score
-    push!(covers.variants, cover)
-    # set the set scores
-    for setix in eachindex(cover.weights)
-        if cover.weights[setix] > 0.0
-            orig_setix = covers.mosaic.setixs[setix]
-            tile_ixs = find(covers.mosaic.tileXset[:, setix])
-            #println("prob[$real_setix]=$(cover.weights[setix])")
-            covers.setscores[orig_setix] = setscore(covers.mosaic.original.set_sizes[orig_setix], nmasked_perset(covers.mosaic)[setix],
-                                                    nelements(covers.mosaic), nmasked(covers.mosaic),
-                                                    problem.params) + log(cover.weights[setix]) + delta_score
-        end
+function _setscore(covers::CoverCollection, cover::CoverProblemResult, problem::CoverProblem, setix::Int)
+    if cover.weights[setix] > 0.0
+        orig_setix = covers.mosaic.setixs[setix]
+        delta_score = isempty(covers.variants) ? 0.0 : cover.score - covers.variants[1].score
+        setscore(covers.mosaic.original.set_sizes[orig_setix], nmasked_perset(covers.mosaic)[setix],
+                nelements(covers.mosaic), nmasked(covers.mosaic),
+                problem.params) - log(cover.weights[setix]) + delta_score
+    else
+        Inf
     end
-    return covers
 end
+
 Base.length(covers::CoverCollection) = length(covers.variants)
 Base.isempty(covers::CoverCollection) = isempty(covers.variants)
 
@@ -61,11 +57,11 @@ end
   Greedy enumeration of coverings.
   Sets selected at each iteration are removed from the collection.
 """
-function Base.collect{T,S}(etor::CoverEnumerator{T,S}; max_covers::Int = 0, max_score_delta::Real = 10.0)
+function Base.collect{T,S}(etor::CoverEnumerator{T,S}; max_covers::Int = 0, max_set_score::Real = -10.0, max_cover_score_delta::Real = 1.0)
     orig_problem = CoverProblem(etor.mosaic, etor.params)
     cur_mosaic = copy(etor.mosaic)
     res = CoverCollection{T,S}(etor.mosaic)
-    best_score = NaN
+    best_cover_score = NaN
     while max_covers == 0 || length(res) < max_covers
         cur_problem = CoverProblem(cur_mosaic, etor.params)
         cur_cover = optimize(cur_problem; ini_weights=rand(nsets(cur_problem)),
@@ -76,13 +72,24 @@ function Base.collect{T,S}(etor::CoverEnumerator{T,S}; max_covers::Int = 0, max_
         # get the weights of sets in the original etor.mosaic problem
         orig_weights = fill(0.0, nsets(etor.mosaic))
         orig_weights[Int[findfirst(etor.mosaic.setixs, setix) for setix in cur_mosaic.setixs]] = cur_cover.weights
+        set_scores = [_setscore(res, cur_cover, cur_problem, i) for i in eachindex(cur_cover.weights)]
         cover_score = score(orig_problem, orig_weights)
         if isempty(res)
-            best_score = cover_score
-        elseif max_score_delta > 0.0 && best_score - cover_score > max_score_delta
+            best_cover_score = cover_score
+        elseif max_cover_score_delta > 0.0 && cover_score - best_cover_score > max_cover_score_delta
             break # bad cover score
         end
-        push!(res, CoverProblemResult(orig_weights, cover_score), cur_problem)
+        if isfinite(max_set_score) && (minimum(set_scores) > max_set_score)
+            break # no set with good score
+        end
+        # save current cover
+        push!(res.variants, CoverProblemResult(orig_weights, cover_score))
+        # update the set scores
+        for (setix, score) in enumerate(set_scores)
+            if isfinite(score)
+                res.setscores[cur_mosaic.setixs[setix]] = score
+            end
+        end
         if max_covers > 0 && length(res) >= max_covers
             break # collected enough covers
         end
--- test/test_cover_enumerator.jl
+++ test/test_cover_enumerator.jl
@@ -6,12 +6,14 @@ facts("CoverEnumerator") do # FIXME use weights
 
         # low prior probability to select sets, high probability to miss active element, so select abc
         cover_etor = OESC.CoverEnumerator(sm_ab, OESC.CoverParams(0.5, 0.1))
-        cover_coll = collect(cover_etor)
+        cover_coll = collect(cover_etor; max_set_score=0.0)
         @fact length(cover_coll) --> 1
+        cover_coll3 = collect(cover_etor; max_set_score=-1.0)
+        @fact length(cover_coll3) --> 0
 
         # higher prior probability to select sets, lower probability to miss active element, so select a and b, then a b c
         cover_etor2 = OESC.CoverEnumerator(sm_ab, OESC.CoverParams(0.1, 0.1))
-        cover_coll2 = collect(cover_etor2)
+        cover_coll2 = collect(cover_etor2; max_set_score=0.0)
         @pending length(cover_coll2) --> 2
     end
 end
