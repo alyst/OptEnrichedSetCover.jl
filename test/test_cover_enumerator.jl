c01c356cd47cda62d7909e7a02ba2060e5cd499d
Alexey Stukalov <astukalov@gmail.com>
2016-02-18 16:09:44 +0100

    [+] simple CoverColl -> DataFrame test

--- test/test_cover_enumerator.jl
+++ test/test_cover_enumerator.jl
@@ -27,4 +27,19 @@ facts("CoverEnumerator") do # FIXME use weights
         @fact length(cover_coll2) --> 2
         @fact cover_coll2.variants[1].score --> less_than_or_equal(cover_coll2.variants[2].score)
     end
+
+    context("convert(DataFrame)") do
+        sm = OESC.SetMosaic([Set([:a]), Set([:b]), Set([:c]), Set([:a, :b, :c])],
+                            Set([:a, :b, :c, :d, :e]))
+        sm_ab = OESC.mask(sm, Set(Symbol[:a, :b]))
+
+        # higher prior probability to select sets, lower probability to miss active element, so select a and b, then a b c
+        cover_etor2 = OESC.CoverEnumerator(sm_ab, OESC.CoverParams(0.1, 0.1))
+        cover_coll2 = collect(cover_etor2; max_set_score=0.0, max_cover_score_delta=0.0)
+
+        df = convert(DataFrame, cover_coll2, sm)
+        @fact size(df, 1) --> 3
+        @fact df[:cover_ix] --> [1, 1, 2]
+        @fact df[:set_id] --> [1, 2, 4]
+    end
 end
