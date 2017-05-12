193ab7f04d571ac542680c1793e76a9a5653ecdc
Alexey Stukalov <astukalov@gmail.com>
2017-05-12 17:07:26 +0200

    [*] logpvalue(): comments

--- set_score.jl
+++ set_score.jl
@@ -6,19 +6,21 @@ function logpvalue(a_size::Integer, b_size::Integer,
                    tail::Symbol = :left)
     ((a_size >= 0) && (b_size >= 0) && (all_size >= 0)) || throw(ArgumentError("Sets with negative number of elements"))
     ((a_size <= all_size) && (b_size <= all_size)) || throw(ArgumentError("Sets bigger that total number of elements"))
+    # corner cases
     if isect_size >= min(a_size, b_size)
         return tail == :left ? 0.0 : -Inf
     elseif isect_size < min(0, a_size + b_size - all_size)
         return tail == :left ? -Inf : 0.0
     end
+    # normal cases
     distr = Distributions.Hypergeometric(a_size, all_size - a_size, b_size)
     if tail == :left
-        logccdf(distr, isect_size-1)
+        return logccdf(distr, isect_size-1)
     elseif tail == :right
-        logcdf(distr, isect_size)
+        return logcdf(distr, isect_size)
     elseif tail == :both
-        min(2.0*min(logcdf(distr, isect_size),
-            logccdf(distr, isect_size-1)), 1.0)
+        return 2.0 * min(logcdf(distr, isect_size),
+                         logccdf(distr, isect_size-1), 0.5)
     else
         throw(ArgumentError("Unsupported tail specifier ($tail)"))
     end
