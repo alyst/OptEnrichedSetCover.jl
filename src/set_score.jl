0f582054215e642126941198d1b91f935508e042
Alexey Stukalov <astukalov@gmail.com>
2017-05-15 21:20:52 +0200

    [*] logpscore(): shorter var names

--- src/set_score.jl
+++ src/set_score.jl
@@ -1,35 +1,33 @@
 """
 Log P-value for the two sets intersection.
 
-`A` has `a_size` elemnts, B has `b_size` elements,
-they have `isect_size` elements in common, and there are
-`all_size` elements in the "universe" in total.
+`A` has `na` elemnts, B has `nb` elements,
+they have `nisect` elements in common, and there are
+`ntotal` elements in the "universe".
 
 `tail` controls the null hypothesis:
-* `:right` (default): by chance `A` and `B` would have more elements in common
-* `:left`: by chance `A` and `B` would have less elements in common
-* `:both`: by chance `A` and `B` would have either more or less elements in common
+* `:right` (default): by chance `A` and `B` would have ≥ elements in common
+* `:left`: by chance `A` and `B` would have ≤ elements in common
+* `:both`: by chance `A` and `B` would have either ≤ or ≥ elements in common, whichever is less probable
 """
-function logpvalue(a_size::Integer, b_size::Integer,
-                   all_size::Integer, isect_size::Integer,
+function logpvalue(na::Integer, nb::Integer, ntotal::Integer, nisect::Integer,
                    tail::Symbol = :right)
-    ((a_size >= 0) && (b_size >= 0) && (all_size >= 0)) || throw(ArgumentError("Sets with negative number of elements"))
-    ((a_size <= all_size) && (b_size <= all_size)) || throw(ArgumentError("Sets bigger than total number of elements"))
+    ((na >= 0) && (nb >= 0) && (ntotal >= 0)) || throw(ArgumentError("Sets with negative number of elements"))
+    ((na <= ntotal) && (nb <= ntotal)) || throw(ArgumentError("Sets bigger than total number of elements"))
     # corner cases
-    if isect_size > min(a_size, b_size)
+    if nisect > min(na, nb)
         return tail == :right ? -Inf : 0.0
-    elseif isect_size < min(0, a_size + b_size - all_size)
+    elseif nisect < min(0, na + nb - ntotal)
         return tail == :left ? 0.0 : -Inf
     end
     # normal cases
-    distr = Distributions.Hypergeometric(a_size, all_size - a_size, b_size)
+    distr = Distributions.Hypergeometric(na, ntotal - na, nb)
     if tail == :right
-        return logccdf(distr, isect_size-1)
+        return logccdf(distr, nisect-1)
     elseif tail == :left
-        return logcdf(distr, isect_size)
+        return logcdf(distr, nisect)
     elseif tail == :both
-        return 2.0 * min(logcdf(distr, isect_size),
-                         logccdf(distr, isect_size-1), 0.5)
+        return 2.0 * min(logcdf(distr, nisect), logccdf(distr, nisect-1), 0.5)
     else
         throw(ArgumentError("Unsupported tail specifier ($tail)"))
     end
