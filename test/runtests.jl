0e9bf6451828496d690f44c0c4f977f33fd15aa4
Alexey Stukalov <astukalov@gmail.com>
2018-01-04 00:16:02 +0000

    [+] test_masked_mosaic.jl: moved masked tests here

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ my_tests = [
     "test_set_score.jl",
     "test_sparse_mask_matrix.jl",
     "test_mosaic.jl",
+    "test_masked_mosaic.jl",
     "test_cover_problem.jl",
     "test_cover_enumerator.jl",
     "test_parallel.jl"
